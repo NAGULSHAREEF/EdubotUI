7ec2a012416044ae13abb00bff8a6c01ee939f81
Shaik NagulShareef <sns7661907634@gmail.com>
2024-09-30 15:44:48 +0530

    Initial commit

---
+++ Edubot_FrontEndPage/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-600">EBUBOT</h1>
+      <h1 className="text-xl font-bold text-red-600">Hello World</h1>
+    </>
+  )
+}
+
+export default App
