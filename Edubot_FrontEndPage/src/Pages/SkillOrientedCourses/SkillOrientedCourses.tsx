e8c66f6df466fe7a3bfe5483d43ff8f8108afac7
Shaik NagulShareef <sns7661907634@gmail.com>
2024-12-08 23:18:22 +0530

    added skillsOreinted in incubation center branch

---
+++ Edubot_FrontEndPage/src/Pages/SkillOrientedCourses/SkillOrientedCourses.tsx
@@ -0,0 +1,15 @@
+import Courses from "./Courses"
+import HeroSection from "./HeroSection"
+import MasteringSkill from "./MasteringSkill"
+
+const SkillOrientedCourses = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <MasteringSkill/>
+      <Courses/>
+    </div>
+  )
+}
+
+export default SkillOrientedCourses
